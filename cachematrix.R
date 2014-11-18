6c8d83e836a2baf11c28157b3a966134af478cd9
ticos5024 <ticos5024@gmail.com>
2014-11-18 17:41:23 -0600

    primerocambior

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Put comments here nose that give an overall description of what your
+## Put comments here nose 2 dfghjkldfghjklsdfghjk that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
